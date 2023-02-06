f871bbbec6693e06b2db68fbb98b9468409f1da2
Orange Robotics <orangerobotics@gmail.com>
2023-02-06 10:54:06 -0500

    Removed Addressable LEDs from the LED subsystem.
    If we end up using them over the Blinkin, we can always get this code from the commit history :yum:

--- 2023_Robot/src/main/java/frc/robot/Constants.java
+++ 2023_Robot/src/main/java/frc/robot/Constants.java
@@ -13,6 +13,11 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+  public static class GoofyAhhConstants {
+    public static final boolean weWillWin = true;
+    public static final boolean weWillLose = false;
+  }
+
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
--- 2023_Robot/src/main/java/frc/robot/subsystems/LED.java
+++ 2023_Robot/src/main/java/frc/robot/subsystems/LED.java
@@ -4,22 +4,14 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.AddressableLED;
-import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LED extends SubsystemBase {
   // 0 and 1 are the PWM ports on the RoboRIO the Blinkin and LED strips are attached to
   private Spark blinkin = new Spark(0);
-  private AddressableLED pwmLED = new AddressableLED(1);
-  // buffer is equal to LEDs
-  private AddressableLEDBuffer pwmLEDBuffer = new AddressableLEDBuffer(60);
-  public LED() {
-    pwmLED.setLength(pwmLEDBuffer.getLength());
-    pwmLED.setData(pwmLEDBuffer);
-    pwmLED.start();
-  }
+
+  public LED() {}
 
   /**
    * Set the color and blink pattern of the LED strip.
@@ -34,32 +26,6 @@ public class LED extends SubsystemBase {
     if ((value >= -1.0) && (value <= 1.0)) {
       blinkin.set(value);
     }
-  }
-
-  /**
-   * Sets the color value for all pwm LEDs using RGB
-   * @param red the r value [0, 255]
-   * @param green the g value [0, 255]
-   * @param blue the b value [0, 255]
-   */
-  public void pwmGlowRGB(int red, int green, int blue) {
-    for (var i = 0; i < pwmLEDBuffer.getLength(); i++) {
-      pwmLEDBuffer.setRGB(i, red, blue, green);
-    }
-    pwmLED.setData(pwmLEDBuffer);
-  }
-
-  /**
-   * Sets the color value for all pwm LEDs using HSV
-   * @param hue the h value [0, 180)
-   * @param saturation the s value [0, 255]
-   * @param value the v value [0, 255]
-   */
-  public void setpwmHSV(int hue, int saturation, int value) {
-    for (var i = 0; i < pwmLEDBuffer.getLength(); i++) {
-      pwmLEDBuffer.setHSV(i, hue, saturation, value);
-    }
-    pwmLED.setData(pwmLEDBuffer);
   }
 
   @Override
