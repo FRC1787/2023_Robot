dcaac2f52c6fa1bf988ed8f710cc3048dc7fd991
Orange Robotics <orangerobotics@gmail.com>
2023-03-17 15:30:31 -0400

    Edits to PickupCube to prevent allyoops.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -28,6 +28,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -143,7 +144,8 @@ public class RobotContainer {
     //get cube in grabber upon intake release
     controller.rightTrigger().and(inConeMode.negate())
       .onFalse(
-        new PickUpCube(intake, elevatorGrabber, indexer, hatHack)
+        new WaitCommand(0.5).andThen(
+        new PickUpCube(intake, elevatorGrabber, indexer, hatHack))
       );
     
     //eject cone
