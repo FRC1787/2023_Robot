9f9287afcf29d5996e4c58a30976bceb0d90309b
Orange Robotics <orangerobotics@gmail.com>
2023-04-11 16:37:33 -0400

    Investigating a weird issue where there are no reported CAN errors when the upper intake wheels become unplugged from the CAN network

--- src/main/java/frc/robot/subsystems/intakeIndex/Intake.java
+++ src/main/java/frc/robot/subsystems/intakeIndex/Intake.java
@@ -12,6 +12,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
@@ -82,5 +83,8 @@ public class Intake extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    // testing to see if querrying a value from the motor will prompt CAN errors to appear when the motor is unplugged from the CAN network
+    SmartDashboard.putNumber("intakeNormalizedOutput", intakeMotor.getAppliedOutput());
   }
 }
